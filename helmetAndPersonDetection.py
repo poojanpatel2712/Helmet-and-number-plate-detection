218eb84fcf0834ed6eac9a32a52183d25c4f5ead
poojanpatel2712 <112116029@ece.iiitp.ac.in>
2024-03-12 17:29:33 +0530

    Implemented non-maximum suppression for bounding boxes of detected persons and helmets
    Implemented non-maximum suppression for bounding boxes and labels of detected persons and helmets using YOLOv3.

--- helmetAndPersonDetection.py
+++ helmetAndPersonDetection.py
@@ -1,7 +1,13 @@
 import numpy as np
+import pandas as pd
+import os
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.patches as patches
+import pylab as pl
+from PIL import Image
+import streamlit as st
 
 def predict(image_path):
 
@@ -104,3 +110,48 @@ def predict(image_path):
                     class_numbers_for_helmet.append(class_current)
 
     print(bounding_boxes_for_helmet)
+
+    # In[9]:
+
+    results0 = cv2.dnn.NMSBoxes(bounding_boxes_for_person, confidences_for_person, probability_minimum, threshold)
+    # Here "cv2.dnn.NMSBoxes" this functions is used to get the non-maximum supression that means it will
+    # choose that bounding box which has high confidense score.
+
+    print("Bounding Box Paramters : ", bounding_boxes_for_person, "\nConfindence Score :", confidences_for_person,
+          "\nProbability and threshhold value : ", probability_minimum, threshold)
+    # print(results0.flattere())
+    # Here "results0.flatten()" this functions is used to convert multidimentional array into 1D array.
+    
+    if len(results0) > 0:
+        for i in results0.flatten():
+            #         print(bounding_boxes_for_person[i][0],bounding_boxes_for_person[i][1])
+            x_min, y_min = bounding_boxes_for_person[i][0], bounding_boxes_for_person[i][1]
+            box_width, box_height = bounding_boxes_for_person[i][2], bounding_boxes_for_person[i][3]
+            colour_box_current = [int(j) for j in colours0[class_numbers_for_person[i]]]
+            cv2.rectangle(image_input, (x_min, y_min), (x_min + box_width, y_min + box_height), colour_box_current, 5)
+            text_box_current0 = '{}: {:.4f}'.format(labels0[int(class_numbers_for_person[i])], confidences_for_person[i])
+            cv2.putText(image_input, text_box_current0, (x_min, y_min - 7), cv2.FONT_HERSHEY_SIMPLEX, 1.5,
+                        colour_box_current, 5)
+
+    # In[10]:
+
+    results1 = cv2.dnn.NMSBoxes(bounding_boxes_for_helmet, confidences_for_helmet, probability_minimum, threshold)
+    print(confidences_for_helmet)
+    
+    if len(results1) > 0:
+        for i in results1.flatten():
+            x_min, y_min = bounding_boxes_for_helmet[i][0], bounding_boxes_for_helmet[i][1]
+            box_width, box_height = bounding_boxes_for_helmet[i][2], bounding_boxes_for_helmet[i][3]
+            colour_box_current = [int(j) for j in colours1[class_numbers_for_helmet[i]]]
+            cv2.rectangle(image_input, (x_min, y_min), (x_min + box_width, y_min + box_height), colour_box_current, 5)
+            text_box_current1 = '{}: {:.4f}'.format(labels1[int(class_numbers_for_helmet[i])], confidences_for_helmet[i])
+            cv2.putText(image_input, text_box_current1, (x_min, y_min - 7), cv2.FONT_HERSHEY_SIMPLEX, 1.5,
+                        colour_box_current, 5)
+
+    # In[11]:
+
+    plt.rcParams['figure.figsize'] = (10.0, 10.0)
+    image_rgb = cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB)
+
+    # Display the image using Matplotlib
+    return image_rgb
