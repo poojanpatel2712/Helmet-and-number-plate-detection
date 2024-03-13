dd3a00db3928fcb1cf27fd63648aff4724b4c1b8
poojanpatel2712 <112116029@ece.iiitp.ac.in>
2024-03-13 19:08:44 +0530

    Implemented character segmentation for license plate recognition.

--- numberplateDetection.py
+++ numberplateDetection.py
@@ -350,3 +350,124 @@ def predict1(image_path):
         # plt.show()
         break
 
+
+        # In[11]
+    # Taking Negative Again
+    img = 255-img_result
+    plt.imshow(img, 'gray')
+    plt.axis('off')
+    plt.savefig('Car-Plates(Negative).png',bbox_inches = 'tight')
+    # plt.show()
+    #border is black because remember i set the background to be black
+
+    # In[12]
+    # A function to find Contours again in the cropped license plate
+    def find_contours(dimensions, img) :
+
+        # Find all contours in the image
+        cntrs, _ = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Retrieve potential dimensions
+        lower_width = dimensions[0]
+        upper_width = dimensions[1]
+        lower_height = dimensions[2]
+        upper_height = dimensions[3]
+        
+        # Check largest 5 or  15 contours for license plate or character respectively
+        cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
+        
+        ii = cv2.imread('contour.jpg')
+        
+        x_cntr_list = []
+        target_contours = []
+        img_res = []
+        for cntr in cntrs :
+            # detects contour in binary image and returns the coordinates of rectangle enclosing it
+            intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
+            
+            # checking the dimensions of the contour to filter out the characters by contour's size
+            if intWidth > lower_width and intWidth < upper_width and intHeight > lower_height and intHeight < upper_height :
+                x_cntr_list.append(intX) #stores the x coordinate of the character's contour, to used later for indexing the contours
+
+                char_copy = np.zeros((44,24))
+                # extracting each character using the enclosing rectangle's coordinates.
+                char = img[intY:intY+intHeight, intX:intX+intWidth]
+                char = cv2.resize(char, (20, 40))
+                
+                cv2.rectangle(ii, (intX,intY), (intWidth+intX, intY+intHeight), (50,21,200), 2)
+                plt.imshow(ii, cmap='gray')
+                plt.axis('off')
+
+                # Make result formatted for classification: invert colors
+                char = cv2.subtract(255, char)
+
+                # Resize the image to 24x44 with black border
+                char_copy[2:42, 2:22] = char
+                char_copy[0:2, :] = 0
+                char_copy[:, 0:2] = 0
+                char_copy[42:44, :] = 0
+                char_copy[:, 22:24] = 0
+
+                img_res.append(char_copy) # List that stores the character's binary image (unsorted)
+                
+        # Return characters on ascending order with respect to the x-coordinate (most-left character first)
+                
+        # plt.show()
+        # arbitrary function that stores sorted list of character indeces
+        indices = sorted(range(len(x_cntr_list)), key=lambda k: x_cntr_list[k])
+        img_res_copy = []
+        for idx in indices:
+            img_res_copy.append(img_res[idx])# stores character images according to their index
+        img_res = np.array(img_res_copy)
+
+        return img_res
+
+    # In[13]
+    # Find characters in the resulting images
+    def segment_characters(image) :
+
+        # Preprocess cropped license plate image
+        img_lp = cv2.resize(image, (333, 75))
+
+        LP_WIDTH = img_lp.shape[0]
+        LP_HEIGHT = img_lp.shape[1]
+
+        # Make borders white
+        img_lp[0:3,:] = 255
+        img_lp[:,0:3] = 255
+        img_lp[72:75,:] = 255
+        img_lp[:,330:333] = 255
+
+        # Estimations of character contours sizes of cropped license plates
+        dimensions = [LP_WIDTH/6,
+                        LP_WIDTH/2,
+                        LP_HEIGHT/10,
+                        2*LP_HEIGHT/3]
+        plt.imshow(img_lp, cmap='gray')
+        plt.axis('off')
+        # plt.show()
+        cv2.imwrite('contour.jpg', img_lp)
+        
+
+        # Get contours within cropped license plate
+        char_list = find_contours(dimensions, img_lp)
+
+        return char_list
+
+    # In[14]
+    char = segment_characters(img)
+
+    # In[15]
+    plt.style.use('ggplot')
+
+    # In[16]
+    # > Separating the Characters in the Plate to identify them seperatly and using the model definied later for it
+    for i in range(len(char)):
+        plt.subplot(1, len(char), i+1)
+        plt.imshow(char[i], cmap='gray')
+        plt.axis('off')
+    plt.savefig('Car-Plates-Char(Seperated).png',bbox_inches = 'tight')
+
+
+
+
